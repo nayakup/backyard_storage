eccb048c648c10231fb3861bda4ed59878f2a83e
nayakup <upnayak@protonmail.me>
2022-07-03 22:29:52 +0530

    notebooks

---
+++ workbook/nlp_spacy_training.py
@@ -0,0 +1,14 @@
+'''
+
+Documentation: http://spacy.pythonhumanities.com/intro.html
+Source: https://www.youtube.com/watch?v=dIUTsFT2MeQ
+Installation: https://spacy.io/usage
+ 
+'''
+
+import spacy
+
+
+# Load Spacy
+nlp = spacy.load("en_core_web_sm")
+print(nlp)
